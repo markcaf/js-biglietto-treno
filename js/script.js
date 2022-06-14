f165f9e8af7b48efb647c45a0c143db0ef4c5240
markcaf <marcocafassog@gmail.com>
2022-06-14 16:44:29 +0200

    optimized code in calculation and output console

--- js/script.js
+++ js/script.js
@@ -10,11 +10,13 @@ console.log("Prezzo in base ai km: €" + ticketPrice);
 
 //Sconti: 20% minorenne e 40% over 65
 if (userAge < 18){
-    ticketPrice = (ticketPrice) - ((ticketPrice * 20) / 100);
-    console.log("Sconto 20% per minorenni, il prezzo del tuo biglietto è: €" + ticketPrice);
+    ticketPrice = ticketPrice - (ticketPrice * 20) / 100;
+    console.log("Sconto 20% per minorenni");
 }   else if (userAge >= 65){
-        ticketPrice = (ticketPrice) - ((ticketPrice * 40) / 100);
-        console.log("Sconto 40% per over65, il prezzo del tuo biglietto è: €" + ticketPrice);
-    }   else{
-            console.log("Nessuno sconto attivo, il prezzo del tuo biglietto è: €" + ticketPrice);    
-        }
+        ticketPrice = ticketPrice - (ticketPrice * 40) / 100;
+        console.log("Sconto 40% per over65");
+    }
+
+//Output prezzo finale
+ticketPrice = ticketPrice.toFixed(2);
+console.log("Il prezzo del tuo biglietto è: €" + ticketPrice);    
