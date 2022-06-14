d7c732e7611c81f815aee5a122049fb5b517171b
markcaf <marcocafassog@gmail.com>
2022-06-14 16:19:47 +0200

    ticket price calculation displayed in console.log

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Chiedo all'utente i due dati, km da percorrere e l'età
+const userDistance = parseInt( prompt ("Inserisci il numero di chilometri che vuoi percorrere"));
+console.log("Distanza in km: " + userDistance);
+const userAge = parseInt( prompt ("Inserisci la tua età"));
+console.log("Età dell'utente: " + userAge);
+
+//Prezzo del biglietto in base ai km
+let ticketPrice = 0.21 * userDistance;
+console.log("Prezzo in base ai km: €" + ticketPrice);
+
+//Sconti: 20% minorenne e 40% over 65
+if (userAge < 18){
+    ticketPrice = (ticketPrice) - ((ticketPrice * 20) / 100);
+    console.log("Sconto 20% per minorenni, il prezzo del tuo biglietto è: €" + ticketPrice);
+}   else if (userAge >= 65){
+        ticketPrice = (ticketPrice) - ((ticketPrice * 40) / 100);
+        console.log("Sconto 40% per over65, il prezzo del tuo biglietto è: €" + ticketPrice);
+    }   else{
+            console.log("Nessuno sconto attivo, il prezzo del tuo biglietto è: €" + ticketPrice);    
+        }
